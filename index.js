5d53049721c2774089177fa251e232986be8cf2e
Carlos Alberto Argüello Ramírez <innovation@qubits.cloud>
2020-04-15 20:26:46 -0500

    se terminó la creación de /echo #1

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/echo', (req, res) => {
-  console.log('===> endpoint /echo ');
-  res.send(`echo`);
+app.use(express.json())
+
+app.post('/echo', (req, res) => {
+  var echo = req.body.mensaje;
+  res.status(200).send({
+      status: true,
+      mensaje: `escribiste: ${echo}`
+  });
+  console.log(`===> endpoint /echo con ${echo}`);
 });
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('simple API escuchando en el puerto: ', port);
